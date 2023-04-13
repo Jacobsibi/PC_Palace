c1f2ace1b66b5075222e453a97168b27d93c8ce7
pendevx <pendevx@gmail.com>
2023-04-14 06:14:46 +1200

    (Navbar.jsx): Extracted the cart button into a new component     Needs adding on-click functionality
    (SearchBar.jsx): Updated the search to take in a parameter

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,31 +4,20 @@ import { AiOutlineShopping } from 'react-icons/ai'
 import SearchBar from "./SearchBar.jsx";
 import styles from "../styles/Navbar.module.css";
 
-// const Navbar = () => {
-//   return (
-//     <div className="navbar-container">
-//       <p className="logo">
-//         <Link href="/">INSERT ---NAVBAR</Link>
-//       </p>
-
-//       <button type="button" className="cart-icon" onClick="">
-//         <AiOutlineShopping/>
-//         <span className="cart-item-qty">1</span>
-//       </button>
-//     </div>
-//   )
-// }
-
-// export default Navbar
+function CartButton(props) {
+    return (
+        <button className="cart-icon" onClick="">
+            <AiOutlineShopping/>
+            <span className="cart-item-qty">{props.itemAmount}</span>
+        </button>
+    );
+}
 
 export default function Navbar() {
 	return (
         <div className={styles.navbar}>
             <SearchBar />
-            <button className="cart-icon" onClick="">
-                <AiOutlineShopping/>
-                <span className="cart-item-qty">1</span>
-            </button>
+            <CartButton itemAmount={1} />
         </div>
     );
 }
--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function search() {
+function search(query) {
 
 }
 
