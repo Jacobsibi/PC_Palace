2d0a1de3243b20415f32dd8d6d34a9bcc9daad9c
pendevx <pendevx@gmail.com>
2023-04-14 16:56:33 +1200

    Refactored Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,45 +1,30 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-<<<<<<< Updated upstream
 import { AiOutlineShopping } from 'react-icons/ai'
 import SearchBar from "./SearchBar.jsx";
 import styles from "../styles/Navbar.module.css";
-
-function CartButton(props) {
-    return (
-        <button className="cart-icon" onClick="">
-            <AiOutlineShopping/>
-            <span className="cart-item-qty">{props.itemAmount}</span>
-        </button>
-    );
-=======
-import { AiOutlineShopping } from 'react-icons/ai';
 import { Cart } from './';
 import { useStateContext } from '../context/StateContext';
 
+const CartButton = () => {
+	const { setShowCart, totalQuantities } = useStateContext();
+	return (<>
+		<button className="cart-icon" onClick={() => setShowCart(true)}>
+			<AiOutlineShopping />
+			<span className="cart-item-qty">{totalQuantities}</span>
+		</button>
+	</>);
+}
+
 const Navbar = () => {
-  const { showCart, setShowCart, totalQuantities} = useStateContext();
-  return (
-    <div className="navbar-container">
-      <p className="logo">
-        <Link href="/">INSERT ---NAVBAR</Link>
-      </p>
-
-      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
-        <AiOutlineShopping/>
-        <span className="cart-item-qty">{totalQuantities}</span>
-      </button>
-      {showCart && <Cart />}
-    </div>
-  )
->>>>>>> Stashed changes
+	const { showCart } = useStateContext();
+	return (<>
+		<div className={styles.navbar}>
+			<SearchBar />
+			<CartButton itemAmount={1} />
+		</div>
+		{showCart && <Cart />}
+	</>);
 }
 
-export default function Navbar() {
-	return (
-        <div className={styles.navbar}>
-            <SearchBar />
-            <CartButton itemAmount={1} />
-        </div>
-    );
-}
+export default Navbar;
