491a4eee6ce2c1476e5ea0d1ae3351f6919ce9a3
Jacob <jacobssibi@gmail.com>
2023-04-14 16:11:03 +1200

    cart navbar changes
    Navbar cart logic changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+<<<<<<< Updated upstream
 import { AiOutlineShopping } from 'react-icons/ai'
 import SearchBar from "./SearchBar.jsx";
 import styles from "../styles/Navbar.module.css";
@@ -11,6 +12,27 @@ function CartButton(props) {
             <span className="cart-item-qty">{props.itemAmount}</span>
         </button>
     );
+=======
+import { AiOutlineShopping } from 'react-icons/ai';
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
+
+const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities} = useStateContext();
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">INSERT ---NAVBAR</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping/>
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+      {showCart && <Cart />}
+    </div>
+  )
+>>>>>>> Stashed changes
 }
 
 export default function Navbar() {
