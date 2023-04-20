60ee9ca404729cc8fe3d651e234eb8bc06a7c7fb
Daniel Lip <gxc1998@autuni.ac.nz>
2023-04-20 12:07:28 +1200

    Login Component created

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,23 +1,31 @@
-import React, { useRef } from "react";
-import Link from "next/link";
-import { AiOutlineLeft, AiOutlineLogin } from "react-icons/ai";
-import { useStateContext } from "../context/StateContext";
-import { urlFor } from "../LIB/client";
+//I need to make names into Login not Cart
+//My goal is to make this pop up when click the little man on homepage
+
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { AiOutlineLeft, AiOutlineLogin } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import toast from 'react-hot-toast';
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '../LIB/client';
+import { useState } from "react";
 import { auth, authGoogle } from "../configurations/Firebase";
 import {
   createUserWithEmailAndPassword,
   signInWithPopup,
   signOut,
 } from "firebase/auth";
-import { useState } from "react";
 
 const Login = () => {
+  
+  //email and password to be used as parameter for Firebase special function
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  //just for fun
+  //output current user email on console
   console.log(auth?.currentUser?.email);
 
+  //calling Firebase special functions
   const signIn = async () => {
     try {
       await createUserWithEmailAndPassword(auth, email, password);
@@ -40,60 +48,67 @@ const Login = () => {
     }
   };
 
+  //internal configurations
   const loginRef = useRef();
-  const { setShowCart } = useStateContext();
+  const {setShowLogin} = useStateContext();
+
   return (
-    <div className="login-wrapper" ref={loginRef}>
-      <div className="login-container">
-        {/*The back button hiding as a text */}
-        <button
+    <div className="cart-wrapper" ref={loginRef}>
+      <div className="cart-container">
+      <button
           type="button"
           className="cart-heading"
-          onClick={() => setShowCart(false)}
-        >
+          onClick={() => setShowLogin(false)}>
           <AiOutlineLeft />
           <span className="heading">Back</span>
         </button>
 
-        {/*The space where user will type in information*/}
-        {
-          <div className="input-space">
-            <AiOutlineLogin size={100} />
-            <pre>
-              <input
-                placeholder="Email..."
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </pre>
-
+        {(
+          <div className="empty-cart">
+            <AiOutlineLogin size={150} />
+            <h3>Welcome to PC Palace</h3>
+            <Link href="/">
             <input
+                placeholder="Email..."
+                onChange={console.log}
+                //onChange={(e) => setEmail(e.target.value)}
+              />
+               <input
               placeholder="Password..."
               type="password"
               onChange={(e) => setPassword(e.target.value)}
+               //onChange={(e) => setPassword(e.target.value)}
             />
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                //onClick={signIn}
+                className="btn"
+              >
+                Sign In
+              </button>
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                //onClick={signInGoogle}
+                className="btn"
+              >
+                Sign In With Google
+              </button>
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                //onClick={logOut}
+                className="btn"
+              >
+                Sign Out
+              </button>
+            </Link>
           </div>
-        }
-
-        {/*The main body and icon  */}
-        {/*maybe check if no one is logged in then show this*/}
-        {
-          <div className="authentication-cart">
-            <h3>Welcome to PC Palace</h3>
-
-            <button className="btn" type="button" onClick={signIn}>
-              Sign In
-            </button>
-            <button className="btn" type="button" onClick={signInGoogle}>
-              Sign In With Google
-            </button>
-            <button className="btn" type="button" onClick={logOut}>
-              Sign Out
-            </button>
-          </div>
-        }
+        )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
