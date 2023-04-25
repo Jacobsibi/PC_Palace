221f8cd5cba4b8275add4a545c69e205b87fee05
Jacob <jacobssibi@gmail.com>
2023-04-26 08:17:40 +1200

    Success Page fix
    Success Page bug-fix

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -38,8 +38,8 @@ export default async function handler(req, res) {
           }
         }),
 
-        success_url: `${req.headers.origin}/?success`,
-        cancel_url: `${req.headers.origin}/?canceled`,
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
       }
       
       const session = await stripe.checkout.sessions.create(params);
