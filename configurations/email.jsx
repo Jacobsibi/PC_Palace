21ce7693508f2bd8300b64b6b539de21348b77f2
Daniel Lip <gxc1998@autuni.ac.nz>
2023-05-01 15:00:50 +1200

    Showing the name (login with Google)

--- configurations/email.jsx
+++ configurations/email.jsx
@@ -1,15 +1,11 @@
 import emailjs from 'emailjs-com';
 
-export const sendEmail (e)  => {
+ const sendEmail = () => {
+    return(
+        console.log("from configurations/email")
+    );
 
-    e.preventDefault();
-  
-    emailjs.sendForm('service_c67h00a', 'template_9l0na6s', e.target, 'gxc1998@autuni.ac.nz')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-
+    
 }
   
+export default sendEmail;
