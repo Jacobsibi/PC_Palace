6bc080d32f690860354769bb1531a18380c5d3a9
Daniel Lip <gxc1998@autuni.ac.nz>
2023-05-02 00:25:36 +1200

    SUPPORT PAGE IS READY
    > reconfigure the EmailJS Configurations
    > CSS module for this component

--- components/Support.jsx
+++ components/Support.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
-//import emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
+import styles from '../styles/Support.module.css';
 
 const Support = () => {
   const form = useRef();
@@ -7,24 +8,27 @@ const Support = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    // emailjs.sendForm('service_c67h00a', 'template_9l0na6s', form.current, 'H5MoUXv7GX7Lw6FmK')
-    //   .then((result) => {
-    //       console.log(result.text);
-    //   }, (error) => {
-    //       console.log(error.text);
-    //   });
+    emailjs.sendForm('service_vds5qa2', 'template_36ltlst', form.current, 'at1R64fRA37Jqwu7D')
+      .then((result) => {
+          console.log(result.text);
+          swal("Message Sent", "Our customer team will contact you soon", "success");
+      }, (error) => {
+          console.log(error.text);
+      });
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+
+    <form class={styles.form} ref={form} onSubmit={sendEmail}>
+      <label class={styles.label} >Name</label>
+      <input class={styles.input} type="text" name="user_name" required/>
+      <label class={styles.label} >Email</label>
+      <input class={styles.input} type="email" name="user_email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
+      <label class={styles.label}> Message</label>
+      <textarea class={styles.textarea} name="message" required/>
+      <input class={styles.input} type="submit" value="Send" />
     </form>
+  
   );
 };
 
