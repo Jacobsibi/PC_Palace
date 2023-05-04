abe01e81cac4d8f86324d1fa3b6511ed332cd0b6
pendevx <pendevx@gmail.com>
2023-05-05 01:30:19 +1200

    all searchbar and search result stuff; now let the keydown be detected by the searchbar input section and dont forget to preventdefault()

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -4,24 +4,9 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import { client } from "../lib/client";
 import Link from "next/link";
 
-const SearchBox = (props) => {
-    const [ query, setQuery ] = React.useState("");
-
-    const handleKeyDown = e => {
-        if (e.key === "Enter") {
-            props.search(query);
-        }
-    }
-
-    return (
-        <input className={styles.searchBox} type="text" placeholder="Search" 
-            onChange={e => setQuery(e.target.value)}
-            onKeyDown={e => handleKeyDown(e)}/>
-    );
-}
-
 const SearchResults = props => {
     const [ productsMatching, setProductsMatching ] = React.useState([]);
+    const resultsRef = React.createRef();
 
     const query = "*" + props.query + "*";
 
@@ -35,43 +20,100 @@ const SearchResults = props => {
         getProductsMatching(query); 
     }, [props.query]);
 
-    return (
-        <div className={styles.queryResults}>
-            <ul>
-                {
-                    productsMatching.map((product, index) => (
-                        <Link href={`product/${product.slug.current}`}>
-                            <div className={styles.resultItem} key={index}>{product.name}</div>
-                        </Link>
-                    ))
-                }
-            </ul>
-        </div>
-    );
-}
+    React.useEffect(() => {
+        function handleClickOutside(e) {
+            if (resultsRef.current && !resultsRef.current.contains(e.target)) {
+                props.resultsUnfocused();
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+    });
+
+    const onKeyDown = event => {
+        if (event.key === "Enter") {
+            props.enterPressed(productsMatching[props.selectedIndex]);
+        } else if (event.key === "ArrowDown") {
+            props.selectedIndex < productsMatching.length ? props.incrementIndex() : console.log("Nothing below!");
+        } else if (event.key === "ArrowUp") {
+            props.selectedIndex > 0 ? props.decrementIndex() : console.log("Nothing above!");
+        }
+    };
+
+    if (productsMatching) {
+        return (
+            <div className={styles.queryResults} ref={resultsRef} onKeyDown={e => onKeyDown(e)}>
+                <ul>
+                    {
+                        productsMatching.map((product, index) => (
+                            <Link href={`product/${product.slug.current}`} onMouseEnter={() => props.setSelectedIndex(index)}>
+                                <div className={`${props.selectedIndex == index ? styles.selected : ""} ${styles.resultItem}`} key={index}>
+                                    {product.name}
+                                </div>
+                            </Link>
+                        ))
+                    }
+                </ul>
+            </div>
+        );
+    }
+    
+    return <></>;
+};
 
 const SearchBar = () => {
     const [ showSearchResults, setShowSearchResults ] = React.useState(false);
     const [ query, setQuery ] = React.useState("");
+    const searchBarRef = React.createRef();
+    const [ resultsFocused, setResultsFocused ] = React.useState(false);
+    const [ searchbarFocused, setSearchbarFocused ] = React.useState(false);
+    const [ selectedResultIndex, setSelectedResultIndex ] = React.useState(0);
 
-    const onEnter = (query) => {
-        if (query.trim() === "") {
-            setShowSearchResults(false);
-        } else {
-            setShowSearchResults(true);
+    React.useEffect(() => {
+        function handleClickOutside(event) {
+            if (searchBarRef.current && !searchBarRef.current.contains(event.target)) {
+                setSearchbarFocused(false);
+            } else {
+                setSearchbarFocused(true);
+            }
         }
-        setQuery(query);
-    }
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+    })
+
+    const searchButtonClicked = () => {
+
+    };
 
     return (
         <>
-            <div className={styles.searchBar}>
-                <SearchBox search={query => onEnter(query)} />
-                <button className={styles.searchButton} onClick={() => {}}>
+            <div className={styles.searchBar} ref={searchBarRef}>
+                <input className={styles.searchBox} type="text" placeholder="Search" 
+                    onChange={e => {
+                        let newQuery = e.target.value.trim();
+                        if (newQuery !== "") {
+                            setQuery(newQuery);
+                            setShowSearchResults(true);
+                        } else {
+                            setShowSearchResults(false);
+                        }
+                    }}/>
+                <button className={styles.searchButton} onClick={() => searchButtonClicked()}>
                     <AiOutlineSearch />
                 </button>
             </div>
-            {showSearchResults && <SearchResults query={query} />}
+            {(showSearchResults && (resultsFocused || searchbarFocused)) && 
+                <SearchResults query={query} resultsUnfocused={() => setResultsFocused(false)} selectedIndex={selectedResultIndex}
+                    setSelectedIndex={e => setSelectedResultIndex(e)} incrementIndex={() => setSelectedResultIndex(selectedResultIndex + 1)}
+                    decrementIndex={() => setSelectedResultIndex(selectedResultIndex - 1)} />}
         </>
     );
 }
