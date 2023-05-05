5cc46090e434109a17eb4e33e3b42973a65cb136
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-05-05 15:28:35 +1200

    Make input name required

--- components/Login.jsx
+++ components/Login.jsx
@@ -28,42 +28,48 @@ const Login = () => {
 
   //CREATE NEW ACCOUNT WITH EMAIL AND PASSWORD
   const createAccount = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
+    if(!fullName){
+      swal("Enter Name", "Please fill in your name", "warning");
+    }else{
+      try {
+        await createUserWithEmailAndPassword(auth, email, password);
+  
+        //Update user's fullname once account is created
+        updateProfile(auth.currentUser, {displayName: fullName});
+        swal("Welcome", "You created new account", "success");
+      }
+      catch (error) {
+        if (error.code === 'auth/email-already-in-use') {
+          // Handle the email already in use error
+          swal("Email already exists", "Please enter new email", "error");
+        } else if (error.code === 'auth/phone-number-already-exists') {
+          // Handle the phone number already in use error
+          swal("Phone number already exists", "Please enter new phone number", "error");
+        } 
+          else if (error.code === 'auth/invalid-email') {
+          // Handle the invalid email 
+          swal("Invalid Email", "Please fill in correct email address", "warning");
+        }
+          else if (error.code === 'auth/missing-email') {
+          // Handle the email field is empty 
+          swal("Enter Email", "Please fill in email field", "warning");
+        }
+          else if (error.code === 'auth/missing-password') {
+          // Handle the password field is empty 
+          swal("Enter Password", "Please fill in password field", "warning");
+        } 
+          else if (error.code === 'auth/weak-password') {
+          // Handle the weak password
+          swal("Enter Strong Password", "Password should be at least 6 characters", "warning");
+        } 
+          else {
+          // Handle other errors
+          swal("Error", "Please try again",  "error");
+        }
+      }
+    }
 
-      //Update user's fullname once account is created
-      updateProfile(auth.currentUser, {displayName: fullName});
-      swal("Welcome", "You created new account", "success");
-    }
-    catch (error) {
-      if (error.code === 'auth/email-already-in-use') {
-        // Handle the email already in use error
-        swal("Email already exists", "Please enter new email", "error");
-      } else if (error.code === 'auth/phone-number-already-exists') {
-        // Handle the phone number already in use error
-        swal("Phone number already exists", "Please enter new phone number", "error");
-      } 
-        else if (error.code === 'auth/invalid-email') {
-        // Handle the invalid email 
-        swal("Invalid Email", "Please fill in correct email address", "warning");
-      }
-        else if (error.code === 'auth/missing-email') {
-        // Handle the email field is empty 
-        swal("Enter Email", "Please fill in email field", "warning");
-      }
-        else if (error.code === 'auth/missing-password') {
-        // Handle the password field is empty 
-        swal("Enter Password", "Please fill in password field", "warning");
-      } 
-        else if (error.code === 'auth/weak-password') {
-        // Handle the weak password
-        swal("Enter Strong Password", "Password should be at least 6 characters", "warning");
-      } 
-        else {
-        // Handle other errors
-        swal("Error", "Please try again",  "error");
-      }
-    }
+    
 
   }
 
