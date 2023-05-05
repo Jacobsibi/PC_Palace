45b3986ff5a8d2b30c98a3dd7aca685e51990a1e
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-05-05 20:18:12 +1200

    Bug fixes Support components
    handle errors properly

--- components/Support.jsx
+++ components/Support.jsx
@@ -5,18 +5,21 @@ import styles from '../styles/Support.module.css';
 const Support = () => {
   const form = useRef();
 
+  //Functinon: send mail using emailJs
   const sendEmail = (e) => {
     e.preventDefault();
 
+
+    // using the imported function to send form, with the address configurations set up
     emailjs.sendForm('service_vds5qa2', 'template_36ltlst', form.current, 'at1R64fRA37Jqwu7D')
       .then((result) => {
-          console.log(result.text);
           swal("Message Sent", "Our customer team will contact you soon", "success");
       }, (error) => {
-          console.log(error.text);
+        swal("Message Not Sent", "Please try again", "error");
       });
   };
 
+  //output
   return (
 
     <form class={styles.form} ref={form} onSubmit={sendEmail}>
