b28e814f717630d1a69caa3678b37d1b045893b5
Jacob <jacobssibi@gmail.com>
2023-05-05 20:47:39 +1200

    banner redesign

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -8,18 +8,15 @@ const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTi
     <div className="footer-banner-container">
       <div className="banner-desc">
         <div className="left">
-          <p>{discount}</p>
           <h3>{largeText1}</h3>
           <h3>{largeText2}</h3>
-          <p>{saleTime}</p>
         </div>
         <div className="right">
-          <p>{smallText}</p>
+          <p>{discount} {smallText}</p>
           <h3>{midText}</h3>
-          <p>{desc}</p>
-          <Link href={`/product/${product}`}>
-            <button type="button">{buttonText}</button>
-          </Link>
+          <p>{desc}</p> <br/>
+          <p>{saleTime}</p>
+
         </div>
 
         <img 
