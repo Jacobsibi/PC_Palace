8191a0c585b6106d60b4162ee5fe580c8f1d0b1d
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-05-11 18:31:03 +1200

    bug fix
    Support Component
    
    fix so that customer only send message if the message box has been filled

--- components/Support.jsx
+++ components/Support.jsx
@@ -1,37 +1,51 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import styles from '../styles/Support.module.css';
+import { sendForm } from 'emailjs-com';
 
 const Support = () => {
+
+  const [message, setMessage] = useState("");
+
   const form = useRef();
 
-  //Functinon: send mail using emailJs
-  const sendEmail = (e) => {
-    e.preventDefault();
 
+    //Function: send mail using emailJs
+    const sendEmail = async (e) => {
+      e.preventDefault();
+  
+  
+      //check empty message
+      if (message.trim() == "") {
+            //true: then show error
+            swal("Enter a message", "Message box cannot be left blank", "warning");
+          } else {
+            //false: then send the message
 
-    // using the imported function to send form, with the address configurations set up
-    emailjs.sendForm('service_vds5qa2', 'template_36ltlst', form.current, 'at1R64fRA37Jqwu7D')
+      // using the imported function to send form, with the address configurations set up from EmailJs and handle the errors as well
+      emailjs.sendForm('service_vds5qa2', 'template_36ltlst', form.current, 'at1R64fRA37Jqwu7D')
       .then((result) => {
           swal("Message Sent", "Our customer team will contact you soon", "success");
       }, (error) => {
         swal("Message Not Sent", "Please try again", "error");
       });
-  };
+          }
+
+
+    };
+
 
   //output
   return (
-
     <form class={styles.form} ref={form} onSubmit={sendEmail}>
       <label class={styles.label} >Name</label>
-      <input class={styles.input} type="text" name="user_name" required/>
+      <input class={styles.input} type="text" name="user_name" maxLength={20} required/>
       <label class={styles.label} >Email</label>
       <input class={styles.input} type="email" name="user_email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
       <label class={styles.label}> Message</label>
-      <textarea class={styles.messageTextarea} name="message" required/>
+      <textarea class={styles.messageTextarea} name="message" onChange={(e) => setMessage(e.target.value)} required pattern="\\S+"/>
       <input class={styles.input} type="submit" value="Send" />
     </form>
-  
   );
 };
 
