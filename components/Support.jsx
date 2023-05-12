f0adc8220e8bbdab18e449a9f6e360137b08152d
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-05-13 11:44:40 +1200

    Update Support.jsx

--- components/Support.jsx
+++ components/Support.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import styles from '../styles/Support.module.css';
-import { sendForm } from 'emailjs-com';
 
 const Support = () => {
 
