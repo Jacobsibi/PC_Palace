3271ec636a0c49e1b6b997d71e3ca96e6ae6e1f8
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-05-16 11:26:09 +1200

    new feature: confirm password when create

--- components/LoginNewAccount.jsx
+++ components/LoginNewAccount.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import swal from "sweetalert";
 import styles from "../styles/Support.module.css";
 import React, { useState, useEffect } from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import { auth } from "../configurations/firebase";
 import { updateProfile } from "firebase/auth";
 import { createUserWithEmailAndPassword } from "firebase/auth";
@@ -13,6 +13,7 @@ const LoginNewAccount = () => {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [fullName, setFullName] = useState("");
 
   //function: refresh the page
@@ -31,55 +32,64 @@ const LoginNewAccount = () => {
           "warning"
         );
       }
-      //Handle the name field is empty
+      //handle the name field is empty
       else if (!fullName) {
         swal("Enter Name", "Please fill in your name", "warning");
         setFullName("");
+        //check if password and confirm password match
+      } else if (password !== confirmPassword) {
+        swal(
+          "Passwords do not match",
+          "Please make sure the passwords match",
+          "warning"
+        );
+        return;
       } else {
+        //create new user
         await createUserWithEmailAndPassword(auth, email, password);
         //update user's fullname once account is created because login with email does not create name automatically
         await updateProfile(auth.currentUser, { displayName: fullName });
         // redirect to home
-        await router.push('/');
+        await router.push("/");
         await swal("Welcome", "You created new account", "success");
         //refresh the page
         await refreshPage();
       }
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
-        // Handle the email already in use error
+        // handle the email already in use error
         swal("Email already exists", "Please enter new email", "error");
       } else if (error.code === "auth/phone-number-already-exists") {
-        // Handle the phone number already in use error
+        // handle the phone number already in use error
         swal(
           "Phone number already exists",
           "Please enter new phone number",
           "error"
         );
       } else if (error.code === "auth/invalid-email") {
-        // Handle the invalid email
+        // handle the invalid email
         swal(
           "Invalid Email",
           "Please fill in correct email address",
           "warning"
         );
       } else if (error.code === "auth/missing-email") {
-        // Handle the email field is empty
+        // handle the email field is empty
         swal("Enter Email", "Please fill in email field", "warning");
         setEmail("");
       } else if (error.code === "auth/missing-password") {
-        // Handle the password field is empty
+        // handle the password field is empty
         swal("Enter Password", "Please fill in password field", "warning");
         setPassword("");
       } else if (error.code === "auth/weak-password") {
-        // Handle the weak password
+        // handle the weak password
         swal(
           "Enter Strong Password",
           "Password should be at least 6 characters",
           "warning"
         );
       } else {
-        // Handle other errors
+        // handle other errors
         swal("Error", "Please try again", "error");
       }
     }
@@ -102,7 +112,7 @@ const LoginNewAccount = () => {
         maxLength={20}
         onChange={(e) => setFullName(e.target.value)}
         onKeyDown={(e) => {
-          if (e.key === 'Enter') {
+          if (e.key === "Enter") {
             createAccount();
           }
         }}
@@ -117,7 +127,7 @@ const LoginNewAccount = () => {
         type="email"
         onChange={(e) => setEmail(e.target.value)}
         onKeyDown={(e) => {
-          if (e.key === 'Enter') {
+          if (e.key === "Enter") {
             createAccount();
           }
         }}
@@ -132,19 +142,31 @@ const LoginNewAccount = () => {
         type="password"
         onChange={(e) => setPassword(e.target.value)}
         onKeyDown={(e) => {
-          if (e.key === 'Enter') {
+          if (e.key === "Enter") {
             createAccount();
           }
         }}
         name="user_password"
         required
       />
+      <label class={styles.label}>Confirm Password</label>
+      <input
+        class={styles.input}
+        type="password"
+        onChange={(e) => setConfirmPassword(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            updateAccount();
+          }
+        }}
+        name="confirm_password"
+      />
 
-        <button class={styles.btn} onClick={createAccount}>
-          Create Account
-        </button>
+      <button class={styles.btn} onClick={createAccount}>
+        Create Account
+      </button>
     </div>
   );
 };
-   
+
 export default LoginNewAccount;
