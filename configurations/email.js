c11abddf1ffa47c09cec537606a45615efe8f84e
pendevx <pendevx@gmail.com>
2023-05-18 23:15:09 +1200

    renamed email.jsx to email.js

---
+++ configurations/email.js
@@ -0,0 +1,16 @@
+import emailjs from 'emailjs-com';
+
+const sendEmail = (e)  => {
+
+    e.preventDefault();
+  
+    emailjs.sendForm('service_c67h00a', 'template_9l0na6s', e.target, 'gxc1998@autuni.ac.nz')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+
+}
+  
+export default sendEmail;
