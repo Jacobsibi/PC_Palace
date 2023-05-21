92662bcc807da758b1f26b29eee8f87157c7bb2c
zwsaqx <zwsaqx@gmail.com>
2023-05-21 16:43:07 +1200

    added slider

--- pages/buildcomputer.js
+++ pages/buildcomputer.js
@@ -10,6 +10,9 @@ const ComputerBuilder = () => {
   const [showStreamingPerformance, setShowStreamingPerformance] = useState(false); // Controls whether to show the gaming performance options
   const [generatedNumber, setGeneratedNumber] = useState(null); // Stores the generated number
 
+
+  const [sliderValue, setSliderValue] = useState(1800); // Stores the slider value
+
   // Event handler for the "Generate" button click
   const handleGenerateClick = () => {
     if (selectedOptions.includes('gaming')) {
@@ -47,6 +50,17 @@ const ComputerBuilder = () => {
     });
   };
 
+  const handleLeagueClick = () => {    //////////////////////////work in progresss
+    if (selectedOptions.includes('low-end-gaming')) {
+      setIsSelected(!isSelected);
+    }
+  }
+      
+  const handleSliderChange = (event) => { // Slider for budget
+    setSliderValue(event.target.value);
+  };
+
+
   // Reset to clear state & refresh page a result. 
   const handleResetClick = () => {
     setSelectedOptions([]);
@@ -61,17 +75,22 @@ const ComputerBuilder = () => {
       // If "Gaming" is not selected and showGamingPerformance is true, hide the gaming performance options
       setShowGamingPerformance(false);
       setShowGamingOptions(false);
-      //reset selectedOptions
-      setSelectedOptions([]);
 
     } else if (selectedOptions.includes('gaming') && !showGamingPerformance) {
       // If "Gaming" is selected and showGamingPerformance is false, show the gaming performance options
       setShowGamingPerformance(true);
     }
 
-    if (!selectedOptions.includes('unsure') && showGamingOptions ) {
+    if (!selectedOptions.includes('gaming') && showGamingOptions) {
+      // If "Gaming" is not selected and showGamingOptions is true, hide the gaming performance options
       setShowGamingOptions(false);
-    } else if (selectedOptions.includes('unsure') && !showGamingOptions && selectedOptions.includes('gaming')) {
+    }
+
+    if (!selectedOptions.includes('unsure') && showGamingOptions ) {    
+      // If "Gaming" is not selected and showGamingOptions is true, hide the gaming performance options
+      setShowGamingOptions(false);
+    } else if (selectedOptions.includes('unsure') && !showGamingOptions && selectedOptions.includes('gaming')) {  
+      // If "Gaming" is selected and showGamingOptions is false, show the gaming performance options
       setShowGamingOptions(true);
     }
 
@@ -87,75 +106,58 @@ const ComputerBuilder = () => {
   return (
     <div style={{ textAlign: 'left', marginLeft: 300, marginRight: 300 }}>
       <h1>Computer Builder</h1>
-      <p>To get started, answer a few simple questions to help us understand what type of computer best fits you!</p>
+      <p>To get started, answer a few simple questions to help us understand what type of computer best fits you!</p> {selectedOptions}
       <br />
 
       <label style={{ fontWeight: 'bold' }}>What will you be using the computer for?</label>
       <br />
 
       <p style={{ fontSize: 23 }}>
-        <input
-          type="checkbox"
-          name="myCheckbox"
-          value="gaming"
-          style={{ width: 25, height: 25, margin: 20 }}
-          onChange={handleOptionChange}
-        />
+        <input type="checkbox" name="myCheckbox" value="gaming" style={{ width: 25, height: 25, margin: 20 }} onChange={handleOptionChange} />
         Gaming
-        <input
-          type="checkbox"
-          name="myCheckbox"
-          value="streaming"
-          style={{ width: 25, height: 25, margin: 20 }}
-          onChange={handleOptionChange}
-        />
+        <input type="checkbox" name="myCheckbox" value="streaming" style={{ width: 25, height: 25, margin: 20 }} onChange={handleOptionChange} />
         Streaming
-        <input
-          type="checkbox"
-          name="myCheckbox"
-          value="workstation"
-          style={{ width: 25, height: 25, margin: 20 }}
-          onChange={handleOptionChange}
-        />
+        <input type="checkbox" name="myCheckbox" value="workstation" style={{ width: 25, height: 25, margin: 20 }} onChange={handleOptionChange} />
         Workstation
-        <input
-          type="checkbox"
-          name="myCheckbox"
-          value="imageEditing"
-          style={{ width: 25, height: 25, margin: 20 }}
-          onChange={handleOptionChange}
-        />
+        <input type="checkbox" name="myCheckbox" value="imageEditing" style={{ width: 25, height: 25, margin: 20 }} onChange={handleOptionChange} />
         Image Editing
       </p>
 
       {showGamingPerformance && (
         <>
           <label style={{ fontWeight: 'bold' }}>What is the desired gaming performance?</label>
-          <p>
-            <input type="radio" className={styles.radio} name="gamingPerformance" value="low-end" onChange={handleOptionChange} style={{ width: 25, height: 25, margin: 20 }} />
-            Low-end
-            <input type="radio" className={styles.radio} name="gamingPerformance" value="medium" onChange={handleOptionChange} style={{ width: 25, height: 25, margin: 20 }} />
+          <p className={styles.radio} >
+            <input type="radio" className={styles.radio}  name="gamingPerformance" value="low-end-gaming" onChange={handleOptionChange} style={{ width: 25, height: 25, margin: 20 }}  />
+            Low-end 
+            <input type="radio" className={styles.radio} name="gamingPerformance" value="medium-gaming" onChange={handleOptionChange} style={{ width: 25, height: 25, margin: 20 }}  />
             Medium
-            <input type="radio" className={styles.radio}name="gamingPerformance" value="high-end" onChange={handleOptionChange} style={{ width: 25, height: 25, margin: 20 }} />
+            <input type="radio" className={styles.radio}name="gamingPerformance" value="high-end-gaming" onChange={handleOptionChange} style={{ width: 25, height: 25, margin: 20 }} />
             High-end 
-            <input type="radio" className={styles.radio}name="gamingPerformance" value="unsure" onChange={handleOptionChange} />
+            <input type="radio" className={styles.radio}name="gamingPerformance" value="unsure" onChange={handleOptionChange} style={{ width: 25, height: 25, margin: 20 }} />
             I dont know
           </p>
-        </>
+        </> 
       )}
 
       {showGamingOptions && (
         <>
-          <label style={{ fontWeight: 'bold' }}>which of these games will you likely play?</label>
-          <p>
-            <input type="radio" name="gamingOptions" value="low-end-gaming" onChange={handleOptionChange} />
-            <button onClick={handleOptionChange}> 
-            <Image src={"/leagueoflegends.jpg"} width={150} height={150}></Image></button>
+          <label style={{ fontWeight: 'bold', }}>which of these games will you likely play?</label>
+          <p> 
+            <button name="gamingOptions" value="low-option" onClick={handleLeagueClick} className={styles.btn2} >
+              <Image src={"/leagueoflegends.jpg"} width={150} height={150}></Image></button>
 
-            <input type="radio" name="gamingOptions" value="high-end-gaming" onChange={handleOptionChange} />
-            High-end 
-            <input type="radio" name="gamingOptions" value="unsure" onChange={handleOptionChange} />
-            I'm not sure 
+            <button name="gamingOptions" value="low-option" onClick={handleOptionChange} className={styles.btn2}>
+              <Image src={"/factorio1.jpg"} width={150} height={150}></Image></button>
+
+            <button name="gamingOptions" value="medium-option" onClick={handleOptionChange} className={styles.btn2}>
+              <Image src={"/csgo1.jpg"} width={150} height={150}></Image></button>
+
+            <button name="gamingOptions" value="high-option" onClick={handleOptionChange} className={styles.btn2}>
+              <Image src={"/gtav.jpg"} width={150} height={150} ></Image></button>
+
+            <button name="gamingOptions" value="high-option" onClick={handleOptionChange} className={styles.btn2}>
+              <Image src={"/witcher3.jpg"} width={150} height={150}></Image></button>
+
           </p>
         </>
       )}
@@ -164,21 +166,27 @@ const ComputerBuilder = () => {
         <>
           <label style={{ fontWeight: 'bold' }}>What bitrate will you want to use when streaming?</label>
           <p>
-            <input type="radio" name="streamingPerformance" value="low-end-streaming" onChange={handleOptionChange} />
+            <input type="radio" className={styles.radio} name="streamingPerformance" value="low-end-streaming" onChange={handleOptionChange} style={{ width: 25, height: 25, margin: 20 }}/>
             Standard Definition (SD, 480p)
-            <input type="radio" name="streamingPerformance" value="medium-streaming" onChange={handleOptionChange} />
+            <input type="radio" className={styles.radio} name="streamingPerformance" value="medium-streaming" onChange={handleOptionChange} style={{ width: 25, height: 25, margin: 20 }}/>
             High Definition (HD, 720p)
-            <input type="radio" name="streamingPerformance" value="high-end-streaming" onChange={handleOptionChange} />
-            Ultra High Definition (UHD or 4K, 2160p)
+            <input type="radio" className={styles.radio} name="streamingPerformance" value="high-end-streaming" onChange={handleOptionChange} style={{ width: 25, height: 25, margin: 20 }}/>
+            Ultra High Definition (4K)
           </p>
         </>
       )}
 
-      <button onClick={handleGenerateClick} className={styles.btn} style={{ fontSize: 23, padding: '1rem', width: '45rem' }}>
+      <label style={{ fontWeight: 'bold' }}>What is your desired budget?</label>
+      <p style={{ fontSize: 20 }}>
+      <input type="range" min="1700" max="3000" value={sliderValue} onChange={handleSliderChange} style={{ width: 700, height: 20, margin: 10}} />
+      ${sliderValue}
+      </p>
+
+      <button onClick={handleGenerateClick} className={styles.btn} style={{ fontSize: 23, padding: '1rem', width: '100%' }}>
         Generate
       </button>
 
-      <button onClick={handleResetClick} style={{ fontSize: 23, padding: '1rem', width: '100%', marginTop: '1rem' }}>
+      <button onClick={handleResetClick} className={styles.btn3} style={{ fontSize: 23, padding: '1rem', width: '100%', marginTop: '1rem' }}>
         Reset
       </button>
 
@@ -192,164 +200,3 @@ const ComputerBuilder = () => {
 };
 
 export default ComputerBuilder;
-
-
-
-
-
-
-
-
-/*   old button case method
-
-import React, { useState } from 'react';
-import styles from '../styles/Builder.module.css';
-
-const ComputerBuilder = () => {
-    const [answers, setAnswers] = useState([]); // State to store the answers
-
-    const handleAnswer = (answer) => {
-        setAnswers([...answers, answer]); // Add the selected answer to the answers array
-    };
-
-    const renderQuestion = () => {
-        switch (answers.length) {
-            case 0:
-                return (
-                    <div>
-                        <h2>Question 1:</h2>
-                        <p>What is your primary use for the PC?</p>
-                        <button className={styles.btn} onClick={() => handleAnswer('use1')}>Gaming</button>
-                        <button className={styles.btn} onClick={() => handleAnswer('use2')}>Streaming </button>
-                        <button className={styles.btn} onClick={() => handleAnswer('use3')}>Workstation</button>
-                    </div>
-                );
-        case 1:
-            if (answers[0] == 'use1') return (
-                <div>
-                        <h2>Question 2:</h2>
-                        <p>Which of these games are you looking to play?</p>
-                        <button className={styles.btn} onClick={() => handleAnswer('use1')}>CS:GO (low end)</button>
-                        <button className={styles.btn} onClick={() => handleAnswer('use2')}>Apex Legneds mid </button>
-                        <button className={styles.btn} onClick={() => handleAnswer('use3')}>Rust high</button>
-                    </div>
-            ); else if (answers[0] == 'use2') return (
-                <div>
-                    <h2>Question 2:</h2>
-                    <p>What is your primary use for the PC?</p>
-                    <button className={styles.btn} onClick={() => handleAnswer('use1')}>Gaming</button>
-                    <button className={styles.btn} onClick={() => handleAnswer('use2')}>Streaming </button>
-                    <button className={styles.btn} onClick={() => handleAnswer('use3')}>Workstation</button>
-                </div>
-            ); else return (
-                <p>workstation boring bruh  </p>
-            );
-        case 2:
-            return (
-                <div>
-                    <h2>Question 3:</h2>
-                    <p>What is your primary use for the PC?</p>
-                    <button className={styles.btn} onClick={() => handleAnswer('use1')}>Gaming</button>
-                    <button className={styles.btn} onClick={() => handleAnswer('use2')}>Streaming </button>
-                    <button className={styles.btn} onClick={() => handleAnswer('use3')}>Workstation</button>
-                </div>
-            );
-        case 3:
-            return (
-                <div>
-                    <h2>Question 4:</h2>
-                    <p>What is your primary use for the PC?</p>
-                    <button className={styles.btn} onClick={() => handleAnswer('use1')}>Gaming</button>
-                    <button className={styles.btn} onClick={() => handleAnswer('use2')}>Streaming </button>
-                    <button className={styles.btn} onClick={() => handleAnswer('use3')}>Workstation</button>
-                </div>
-            );
-        case 4:
-            return (
-                <div>
-                    <h2>Question 1:</h2>
-                    <p>What is your budget?</p>
-                    <button className={styles.btn} onClick={() => handleAnswer('budget1')}>Less than $1000</button>
-                    <button className={styles.btn} onClick={() => handleAnswer('budget2')}>$1000 - $1500</button>
-                    <button className={styles.btn} onClick={() => handleAnswer('budget3')}>$1500 - $2000</button>
-                    <p>Note: lowering your budget may result in your PC being garbage. <br/>To prevent this, make sure to select the third option.</p>
-                </div>
-            );
-        case 5:
-            return (
-                <div>
-                    <h2>Question 6:</h2>
-                    <p>What is your primary use for the PC?</p>
-                    <button className={styles.btn} onClick={() => handleAnswer('use1')}>Gaming</button>
-                    <button className={styles.btn} onClick={() => handleAnswer('use2')}>Streaming </button>
-                    <button className={styles.btn} onClick={() => handleAnswer('use3')}>Workstation</button>
-                </div>
-            );
-        case 6:
-            return (
-                <div>
-                    <h2>Question 7:</h2>
-                    <p>What is your primary use for the PC?</p>
-                    <button className={styles.btn} onClick={() => handleAnswer('use1')}>Gaming</button>
-                    <button className={styles.btn} onClick={() => handleAnswer('use2')}>Streaming </button>
-                    <button className={styles.btn} onClick={() => handleAnswer('use3')}>Workstation</button>
-                </div>
-            );
-      
-      default:
-        return (
-          <div>
-            <h2>Question {answers.length + 1}:</h2>
-            <p>Placeholder question</p>
-            <button onClick={() => handleAnswer('placeholder')}>Placeholder option 1</button>
-            <button onClick={() => handleAnswer('placeholder')}>Placeholder option 2</button>
-            <button onClick={() => handleAnswer('placeholder')}>Placeholder option 3</button>
-          </div>
-        );
-    }
-  };
-
-  return (
-    <div style={{ textAlign: 'center', marginLeft :300, marginRight : 300}}>
-        <h1>Computer Builder</h1>   
-        <p>To get started answer a few simple questions to help us unnderstand what type of computer best fits you!</p> <br     /> 
-
-        {renderQuestion()} 
-    
-    </div>
-
-  );
-};
-
-
-export default ComputerBuilder;
-
-/* 
-
-
-Storage
-How much storage will you require
-
-
-CASE
-how important to you is style and aestetics?
-not very important
-somewhat important
-very important?
-
-
-maybe later bc too complex
-LOW MID HIGH TIER FILTER (depending on what they put for this could filter the parts that are low end or high end)
-how much were you looking to spend on this PC?   Slider
-not alot ($500-$1000 NZD)
-regular amount ($1000-$2000 NZD)
-large amount ($2000-$5000 NZD)
-
-
-
-        <button className="big-button">Begin AI Builder</button>
-        <button className="big-button">Custom Build</button>
-
-
-
-*/
