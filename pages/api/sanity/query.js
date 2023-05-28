dcd4b5860fa3f51316bd706916dc516cd2a22df0
pendevx <pendevx@gmail.com>
2023-05-28 23:17:46 +1200

    edited query api to involve error handling

--- pages/api/sanity/query.js
+++ pages/api/sanity/query.js
@@ -1,8 +1,20 @@
 import { client } from "../../../lib/client";
 
 export default async function sanityQuery(req, res) {
+    if (req.method !== "GET") {
+        return res.status(400).end();
+    }
+
+    if (Object.keys(req.query).length === 0) {
+        return res.status(400).end();
+    }
+
     let query = req.query.query;
 
-    const results = await client.fetch(query);
-    res.status(200).json(results);
+    try {
+        const results = await client.fetch(query);
+        return res.status(200).json(results).end();
+    } catch (error) {
+        return res.status(400).end();
+    }
 }
