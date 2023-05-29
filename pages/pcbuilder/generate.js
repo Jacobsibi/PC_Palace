c25d85f6e86deb4e2a371a9165dde5756b69c958
pendevx <pendevx@gmail.com>
2023-05-29 19:42:22 +1200

    added CSS to generate.js and working on generate.js

--- pages/pcbuilder/generate.js
+++ pages/pcbuilder/generate.js
@@ -1,13 +1,41 @@
-import { useRouter } from "next/router";
 import { client } from "../../lib/client";
+import styles from "../../styles/Generate.module.css";
+import { Product } from "../../components/";
 
-const Generate = ({ props }) => {
-    
+const ProductsDisplay = props => {
+    return (
+        <div className={styles.computerParts}>
+            {/* <button>CPU</button>
+            <button>GPU</button>
+            <button>Motherboard</button>
+            <button>RAM</button>
+            <button>Storage</button>
+            <button>Power Supply</button>
+            <button>Cooling</button>
+            <button>Case</button> */}
+            {
+                props.pc.map((current, index) => {
+                    <div className={styles.singleItem}>
+                        <Image src={current.image} />
+                        <button>Edit</button>
+                    </div>
+                })
+            }
+        </div>
+    );
 }
 
-export const getServerSideProps = async () => {
-    const router = useRouter();
-    const params = router.query;
+const Generate = ({ props }) => {
+    return (
+        <div className={styles.contentFixed}>
+            <h2>pc yay :)</h2>
+            <ProductsDisplay pc={[]} />
+        </div>
+    )
+}
+
+export const getServerSideProps = async context => {
+    const query = context?.query;
 
     // fetch PC from sanity
 
