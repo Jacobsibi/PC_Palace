15141e018a4cc982d4383bfce161fef4645314f5
Jacob <jacobssibi@gmail.com>
2023-06-03 14:13:01 +1200

    schema updates

--- sanitypcpalace/schemas/index.js
+++ sanitypcpalace/schemas/index.js
@@ -5,8 +5,7 @@ import gpu from './gpu';
 import motherboard from './motherboard';
 import storage from './storage';
 import PCcase from './case.js';
-import buildLow from './buildLow';
-import buildMed from './buildMed';
-import buildHigh from './buildHigh';
+import builds from './builds'
 
-export const schemaTypes = [product, banner, buildLow, buildMed, buildHigh, cpu, gpu, motherboard, storage, PCcase];
+
+export const schemaTypes = [product, banner, builds, cpu, gpu, motherboard, storage, PCcase];
